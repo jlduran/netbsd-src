e2ba62e980d3572f220e6199a5a5700110cbe297
Jose Luis Duran <jlduran@users.noreply.github.com>
2023-11-30 00:34:50 +0000

    mtree: Fix -f -f not considering type changes
    This only lists the changed type and not other attributes so that it
    matches the behavior of -C.
    
    Obtained from:	https://cgit.freebsd.org/src/commit/?id=4f4b548b1d21ae16955b09f9b717524167a7d97e

--- tests/usr.sbin/mtree/t_mtree.sh
+++ tests/usr.sbin/mtree/t_mtree.sh
@@ -412,6 +412,43 @@ netbsd6_nonemptydir_body()
 }
 
 
+atf_test_case mtree_specspec_type
+mtree_specspec_type_head()
+{
+	atf_set "descr" "Test that spec comparisons detect type changes"
+}
+
+mtree_specspec_type_body()
+{
+	mkdir testdir
+
+	touch testdir/bar
+	mtree -c -p testdir > mtree1.spec
+
+	if [ ! -f mtree1.spec ]; then
+		atf_fail "mtree failed"
+	fi
+
+	rm -f testdir/bar
+	ln -s foo testdir/bar
+	# uid change is expected to be ignored as done in -C
+	chown -h operator testdir/bar
+	mtree -c -p testdir > mtree2.spec
+
+	if [ ! -f mtree2.spec ]; then
+		atf_fail "mtree failed"
+	fi
+
+	atf_check -s ignore -o save:output \
+	    -x "mtree -f mtree1.spec -f mtree2.spec"
+
+	if ! cut -f 3 output | egrep -q "bar file" || \
+	    ! cut -f 3 output | egrep -q "bar link"; then
+		atf_fail "mtree did not detect type change"
+	fi
+}
+
+
 atf_test_case mtree_onlyfile
 atf_test_case netbsd6_onlyfile
 onlyfile_head()
